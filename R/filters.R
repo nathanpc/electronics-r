4f6fbd40f65da058dbca70ea363ae605b7ef63fd
Nathan Campos <eeepc904@gmail.com>
2014-01-11 16:03:24 -0200

    Renamed the RC filter functions

--- R/filters.R
+++ R/filters.R
@@ -5,32 +5,35 @@
 #'
 #' @author Nathan Campos \email{nathanpc@@dreamintech.net}
 
-#' Calculates the cutoff frequency of a RC low-pass filter.
+#' Calculates the cutoff frequency of a RC filter.
 #'
 #' @references
+#' \url{http://en.wikipedia.org/wiki/RC_circuit}
 #' \url{http://en.wikipedia.org/wiki/Low-pass_filter}
+#' \url{http://en.wikipedia.org/wiki/High-pass_filter}
 #'
 #' @param r Resistance (Ohms).
 #' @param c Capacitance (Farads).
 #' @return Cutoff frequency (Hertz).
-filter.rc.lowpass <- function (r, c) {
+filter.rc <- function (r, c) {
   return(1 / (2 * pi * r * c))
 }
 
-#' Calculates resistor value for a low-pass filter.
+#' Calculates resistor value for a RC filter.
 #' 
 #' @param f Frequency (Hertz).
 #' @param c Capacitance (Farads).
 #' @return Resistance (Ohms).
-filter.rc.lowpass.r <- function (f, c) {
+filter.rc.r <- function (f, c) {
   return(1 / (2 * pi * c * f))
 }
 
-#' Calculates capacitor value for a low-pass filter.
+#' Calculates capacitor value for a RC filter.
 #' 
 #' @param f Frequency (Hertz).
 #' @param r Resistance (Ohms).
 #' @return Capacitance (Farads).
-filter.rc.lowpass.c <- function (f, r) {
+filter.rc.c <- function (f, r) {
   return(1 / (2 * pi * r * f))
 }
+
